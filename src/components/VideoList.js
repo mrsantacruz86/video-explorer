a60ffc95f49a8ccd0b9ff4a220efed96c4719cb8
Nelson Diaz <abnerdc86@gmail.com>
2019-06-10 03:20:06 -0400

    fixing warnings #1

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,8 +2,14 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 const VideoList = ({ videos, onVideoSelect }) => {
-  const renderList = videos.map((video,index) => {
-    return <VideoItem  key={index} video={video} onVideoSelect={onVideoSelect} />
+  const renderList = videos.map((video) => {
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        video={video}
+        onVideoSelect={onVideoSelect}
+      />
+    );
   });
 
   return (
