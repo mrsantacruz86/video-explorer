13cc21d2438321915b4c3e219a8ef92b198ace6a
Nelson Diaz <abnerdc86@gmail.com>
2020-01-03 12:49:49 -0500

    fixing issue with the api request address

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const KEY = "AIzaSyD7HyH0Kc5nM8rK8BKl0bEL4wHEuTQgZ6w";
 
 export default axios.create({
-  baseURL: "https:www.googleapis.com/youtube/v3/",
+  baseURL: "https://www.googleapis.com/youtube/v3/",
   params: {
     part: "snippet",
     maxResults: 5,
