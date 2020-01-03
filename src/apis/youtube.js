e6279acbafcaae406e2f581fc14dec7fd2b653f0
Nelson Diaz <abnerdc86@gmail.com>
2020-01-03 10:39:10 -0500

    Fixing the API key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-const KEY = "AIzaSyADijAErdifqiycpj8yDiP8Q7QgyVEyWjs";
+const KEY = "AIzaSyD7HyH0Kc5nM8rK8BKl0bEL4wHEuTQgZ6w";
 
 export default axios.create({
   baseURL: "https:www.googleapis.com/youtube/v3/",
   params: {
-    part:"snippet",
+    part: "snippet",
     maxResults: 5,
-    key: KEY,
+    key: KEY
   }
-})
+});
